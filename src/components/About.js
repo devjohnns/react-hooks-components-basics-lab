ff7ef6346501e2819e8ad2f57be72927cffe971e
john maina <maina202120@gmail.com>
2025-07-22 23:42:30 +0300

    submit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// src/components/About.js
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1>About Me</h1>
+      <p>I’m a developer learning React!</p>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+// src/components/Navbar.js
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <h2>My Portfolio</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
